42e21d38f251b20ff5fbb9c90c4b8953daad899c
Heitor <heitor.scarv@gmail.com>
2026-08-24 16:36:57 -0300

    feat: add external links

---
+++ js/links-config.js
@@ -0,0 +1,8 @@
+const LINKS = {
+    instagram: "https://www.instagram.com/#",
+    whatsapp: "https://whatsapp.com/#",
+    behance: "https://www.behance.com/#",
+    design: "#",
+    portfolioDesign: "#",
+    portfolioSocialMedia: "#",
+}
